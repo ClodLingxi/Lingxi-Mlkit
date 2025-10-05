17913d2880ec460f077ce50a10d4b541e89227cb
Lingxi <2863878907@qq.com>
2025-10-05 15:57:06 +0800

    torch base util

---
+++ test.py
@@ -0,0 +1,74 @@
+from typing import Type
+
+import torch
+from torch import nn
+from torch.nn.functional import one_hot
+
+from sklearn.datasets import load_iris
+from torch.utils.data import TensorDataset
+
+from config import BaseTrainConfig, BaseModelConfig
+from dataset import BaseDataset
+from model import BaseModel
+from train import BaseTrainer
+
+def load_dataset_fn():
+    sklearn_iris_dataset = load_iris()
+    tensor_dataset = TensorDataset(
+        torch.from_numpy(sklearn_iris_dataset.data).float(),
+        one_hot(torch.from_numpy(sklearn_iris_dataset.target)).float(),
+    )
+    return tensor_dataset
+
+
+class ModelConfig(BaseModelConfig):
+    def __init__(
+            self,
+            activation_func: Type[nn.Module] = nn.ReLU,
+            **kwargs
+    ):
+        super().__init__()
+        self.class_num = kwargs.get('class_num', 3)
+        self.sequence_layer = kwargs.get('sequence_layer', [4, 16, self.class_num])
+        self.activation_func = activation_func
+
+
+class Model(BaseModel):
+    def __init__(self, config: ModelConfig):
+        super(Model, self).__init__(config)
+        self.config = config
+        self.loss_fn = nn.CrossEntropyLoss()
+
+        self.model_layer = []
+        for idx in range(len(self.config.sequence_layer) - 1):
+            self.model_layer.append(nn.Linear(self.config.sequence_layer[idx], self.config.sequence_layer[idx + 1]))
+            if idx <= len(self.config.sequence_layer) - 3:
+                self.model_layer.append(self.config.activation_func())
+        self.model_layer = nn.Sequential(*self.model_layer)
+
+    def loss(self, y_true, y_pred):
+        return self.loss_fn(y_true, y_pred)
+
+    def forward(self, batch):
+        x, y = batch
+        pred = self.model_layer(x)
+        loss = self.loss(pred, y)
+
+        acc_metric = (torch.argmax(y, dim=-1) == torch.argmax(pred, dim=-1)).sum() / len(pred)
+
+        return loss, {"acc": acc_metric}
+
+
+if __name__ == '__main__':
+    train_config = BaseTrainConfig()
+    train_config.load_dataset_func = load_dataset_fn
+    train_config.enable_scheduler = False
+
+    train_dataset = BaseDataset(train_config)
+
+    model_config = BaseModelConfig()
+
+
+    trainer = BaseTrainer(dataset=train_dataset, train_config=train_config)
+    trainer.train(Model, ModelConfig())
+
